833447ec317a61ebaf61d9c668ea3ff0f123b2fd
JoshuaAkpan <67443275+JoshuaAkpan@users.noreply.github.com>
2025-10-31 18:14:43 +0100

    Update page.tsx
    link close button to cubic homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import SpinToWin from "@/component/SpinToWin";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="bg-[url('/images/cubic-bg.svg')] bg-cover bg-center min-h-screen flex flex-col items-center justify-center overflow-hidden">
+     <Link href="https://www.cubicprints.africa">
       <div className="cursor-pointer z-10 top-5 right-5 p-4 absolute">
         <Image
           src="/images/close.svg"
@@ -13,6 +15,8 @@ export default function Home() {
           className="object-contain"
         />
       </div>
+     </Link>
+
       <SpinToWin />
     </div>
   );
